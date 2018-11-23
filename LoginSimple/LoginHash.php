ca5c6fbf717c698cfc69f5cb674d2bfd826256ae
nilarrus <nilarrus69@gmail.com>
2018-11-23 17:06:49 +0100

    Login

--- LoginSimple/LoginHash.php
+++ LoginSimple/LoginHash.php
@@ -8,7 +8,7 @@
     <div class="">
       <h4>Login</h4>
 <?php
-    echo "<form class='' action='index.php' method='post'>
+    echo "<form class='' action='LoginHash.php' method='post'>
     <label>User:</label><input type='text' name='user' value=''><br/>
     <label>Password:</label><input type='password' name='paswd' value=''><br/>
     <input type='submit' value='Login'>
@@ -19,8 +19,8 @@
       $pasword = $_POST['paswd'];
       $conn = mysqli_connect('localhost','nil','1234');
       mysqli_select_db($conn,'Login');
-      $paswordSHA =  hash('SHA512',$pasword);
-      $consulta = "SELECT User, Pasword FROM login WHERE User ='$ususario' AND Pasword =SHA2('$pasword',512);";
+      $consulta = "SELECT * FROM login WHERE User ='$ususario' AND Pasword =SHA2('$pasword',512);";
+      var_dump($consulta);
       $resultat = mysqli_query($conn,$consulta);
       if(!$resultat){
         $message  = 'Consulta invàlida: ' . mysqli_error() . "\n";
@@ -29,7 +29,10 @@
       }
       $numColumnas = mysqli_num_rows($resultat);
       if($numColumnas==1){
-        echo "<h2 style='color:green'>Login correcte</h2>";
+        $registre;
+        while ($registre = mysqli_fetch_assoc($resultat)) {
+            echo "<h2 style='color:green'>Login correcte</h2> <br/> <h1 style='background:blue'>Hola:".$ususario."</h1>";
+        }
       }else{
         echo "<h2 style='color:red'>Login Incorrecto</h2>";
       }
